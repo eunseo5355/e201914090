a01757afc1aa73a44b3609c66af88275c9609fe2
eunseo5355 <eunseo5650525@gmail.com>
2022-09-26 17:30:19 +0900

    Hello.java 수정

--- src/e201914090/Hello.java
+++ src/e201914090/Hello.java
@@ -2,13 +2,6 @@ package e201914090;
 
 public class Hello {
 	String msg = "hello";
-<<<<<<< HEAD
-
-	public Hello(String msg) { 
-		this.msg = msg; 
-	} 
-=======
->>>>>>> 8d1e328a561b332b26a8fe59689c014df559159a
 	
 	public void say() { 
 		say(this.msg); 
