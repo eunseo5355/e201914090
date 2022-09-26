34af8c4c8ac8a40fdb84f269e57533913e085eb9
eunseo5355 <eunseo5650525@gmail.com>
2022-09-26 16:59:43 +0900

    최초 커밋

---
+++ src/e201914090/Hello.java
@@ -0,0 +1,8 @@
+package e201914090;
+
+public class Hello {
+
+	public void say() { 
+		System.out.println("hello"); 
+	}
+}
---
+++ src/e201914090/Main.java
@@ -0,0 +1,8 @@
+package e201914090;
+
+public class Main {
+	
+	public static void main(String[] args) { 
+		System.out.println("hello world"); 
+	}
+}
